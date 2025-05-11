fac0e2a008f46684e828657fbe0a0afa5b401b77
symonbaikov <peterbaikov12@gmail.com>
2025-05-11 21:29:04 +0300

    Update package dependencies: added @neondatabase/serverless, dotenv, drizzle-orm, and drizzle-kit; updated existing dependencies in package.json and package-lock.json.

---
+++ lib/db/schema.ts
@@ -0,0 +1,48 @@
+import {pgTable, text, uuid, integer, boolean, timestamp} from "drizzle-orm/pg-core"
+import {relations} from "drizzle-orm"
+
+export const files = pgTable("flies", {
+    id: uuid("id").defaultRandom().primaryKey(),
+
+    //basic file info
+    name: text("name").notNull(),
+    path: text("path").notNull(), //document/project/resum
+    size: integer("size").notNull(),
+    type: text("type").notNull(),
+
+    // storage info
+    fileUrl: text("file_url").notNull(), // url to access file
+    thumbnailUrl: text("thumbnail_url"),
+
+    //Ownership info
+    userId: text("user_id").notNull(),
+    parentId: uuid("parent_id"), //Parent folder id (null for root items)
+    
+    // file/folder flags
+    isFolder: boolean("is_folder").default(false).notNull(),
+    isStarred: boolean("is_starred").default(false).notNull(),
+    isTrash: boolean("is_trash").default(false).notNull(),
+
+    //Timestamps
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+})
+
+/*
+parent : Each file/folder can have one parent folder
+
+children: Each folder can have many child files/folder
+*/
+export const filesRelations = relations(files, ({one, many}) => ({
+    parent: one(files, {
+        fields: [files.parentId],
+        references: [files.id],
+    }),
+    // realatinship to child file/folder
+    children: many(files)
+}))
+
+// Type definations
+
+export const File = typeof files.$inferSelect
+export const NewFile = typeof files.$inferInsert
